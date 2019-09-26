00f54587d43211b4eba40b7118105c9df9f800e9
hpfr <44043764+hpfr@users.noreply.github.com>
2019-09-26 07:57:27 -0500

    Add qemu-kvm setup to server

--- .config/nixos/monolith.nix
+++ .config/nixos/monolith.nix
@@ -5,5 +5,15 @@
     ./base.nix
   ];
 
+  boot.kernelModules = [ "kvm-amd" ]; # required for virtualisation
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+  };
+
+  networking.hostName = "monolith";
+
   services.openssh.enable = true;
+
+  virtualisation.libvirtd.enable = true;
+  users.extraUsers.lh.extraGroups = [ "libvirtd" "kvm" ];
 }
