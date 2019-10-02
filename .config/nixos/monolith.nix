df3bc4d377eecb12ce23cc25cf680f5bdb6d163e
hpfr <44043764+hpfr@users.noreply.github.com>
2019-10-01 21:35:19 -0500

    Add basic server networking config for router VM

--- .config/nixos/monolith.nix
+++ .config/nixos/monolith.nix
@@ -10,10 +10,40 @@
     "net.ipv4.ip_forward" = 1;
   };
 
-  networking.hostName = "monolith";
+  networking = {
+    hostName = "monolith";
+    bridges = {
+      br25 = {
+        interfaces = [ "enp25s0" ];
+      };
+      br26 = {
+        interfaces = [ "enp26s0" ];
+      };
+    };
+    interfaces = {
+      # br25.ipv4.addresses = [
+      #   {
+      #     address = "192.168.1.8";
+      #     prefixLength = 24;
+      #   }
+      # ];
+      br26.ipv4.addresses = [
+        {
+          address = "192.168.1.9";
+          prefixLength = 24;
+        }
+      ];
+    };
+  };
 
-  services.openssh.enable = true;
+  services = {
+    openssh.enable = true;
+  };
 
-  virtualisation.libvirtd.enable = true;
+  virtualisation = {
+    libvirtd.enable = true;
+    # docker.enable = true; # docker screws with networking, use a VM
+  };
+
   users.extraUsers.lh.extraGroups = [ "libvirtd" "kvm" ];
 }
