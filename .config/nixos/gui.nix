500055a2d1abef157d366723a633ab147d7e4f16
hpfr <44043764+hpfr@users.noreply.github.com>
2019-10-06 13:19:55 -0500

    Fix Steam Controller handling, open Steam ports
    Steam needs open ports for in-home streaming and some other features

--- .config/nixos/gui.nix
+++ .config/nixos/gui.nix
@@ -5,6 +5,7 @@
     ./base.nix
   ];
 
+  boot.kernelModules = [ "uinput" ]; # hw.steam-hw should do this automatically soon
   location.provider = "geoclue2"; # for redshift
 
   fonts.fontconfig.penultimate.enable = true;
@@ -18,6 +19,24 @@
     };
     bluetooth.enable = true;
     opengl.driSupport32Bit = true; # for 32-bit games
+    steam-hardware.enable = true;
+  };
+
+  networking.firewall = {
+    allowedTCPPorts = [
+      27036 27037 # steam in-home streaming
+    ];
+    allowedTCPPortRanges = [
+      { from = 27015; to = 27030; } # steam login and download
+    ];
+    allowedUDPPorts = [
+      27031 27036 # steam in-home streaming
+      4380 # steam client?
+    ];
+    allowedUDPPortRanges = [
+      { from = 27015; to = 27030; } # steam login and download
+      { from = 27000; to = 27100; } # steam game traffic
+    ];
   };
 
   services = {
@@ -34,72 +53,7 @@
 
     dbus.packages = with pkgs; [ gnome3.dconf ];
 
-    # for steam controller # FIXME
-    # hardware.steam-hardware.enable = true;
-    # services.udev.extraRules = ''
-    # #   # This rule is needed for basic functionality of the controller in Steam and keyboard/mouse emulation
-    # #   SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
-
-    # # This rule is necessary for gamepad emulation; make sure you replace 'pgriffais' with a group that the user that runs Steam belongs to
-    # #   # KERNEL=="uinput", MODE="0660", GROUP="steam-input", OPTIONS+="static_node=uinput"
-    # #   # KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput"
-    # KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput", GROUP="input", MODE="0660"
-
-    # #   # Valve HID devices over USB hidraw
-    # #   KERNEL=="hidraw*", ATTRS{idVendor}=="28de", MODE="0666"
-
-    # #   # Valve HID devices over bluetooth hidraw
-    # #   KERNEL=="hidraw*", KERNELS=="*28DE:*", MODE="0666"
-
-    # #   # DualShock 4 over USB hidraw
-    # #   KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", MODE="0666"
-
-    # #   # DualShock 4 wireless adapter over USB hidraw
-    # #   KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ba0", MODE="0666"
-
-    # #   # DualShock 4 Slim over USB hidraw
-    # #   KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="09cc", MODE="0666"
-
-    # #   # DualShock 4 over bluetooth hidraw
-    # #   KERNEL=="hidraw*", KERNELS=="*054C:05C4*", MODE="0666"
-
-    # #   # DualShock 4 Slim over bluetooth hidraw
-    # #   KERNEL=="hidraw*", KERNELS=="*054C:09CC*", MODE="0666"
-
-    # #   # Nintendo Switch Pro Controller over USB hidraw
-    # #   KERNEL=="hidraw*", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="2009", MODE="0666"
-
-    # #   # Nintendo Switch Pro Controller over bluetooth hidraw
-    # #   KERNEL=="hidraw*", KERNELS=="*057E:2009*", MODE="0666"
-    # '';
     udev.extraRules = ''
-      # Valve USB devices
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0660", TAG+="uaccess"
-
-      # Steam Controller udev write access
-      KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput"
-
-      # Valve HID devices over USB hidraw
-      KERNEL=="hidraw*", ATTRS{idVendor}=="28de", MODE="0660", TAG+="uaccess"
-
-      # Valve HID devices over bluetooth hidraw
-      KERNEL=="hidraw*", KERNELS=="*28DE:*", MODE="0660", TAG+="uaccess"
-
-      # DualShock 4 over USB hidraw
-      KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", MODE="0660", TAG+="uaccess"
-
-      # DualShock 4 wireless adapter over USB hidraw
-      KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ba0", MODE="0660", TAG+="uaccess"
-
-      # DualShock 4 Slim over USB hidraw
-      KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="09cc", MODE="0660", TAG+="uaccess"
-
-      # DualShock 4 over bluetooth hidraw
-      KERNEL=="hidraw*", KERNELS=="*054C:05C4*", MODE="0660", TAG+="uaccess"
-
-      # DualShock 4 Slim over bluetooth hidraw
-      KERNEL=="hidraw*", KERNELS=="*054C:09CC*", MODE="0660", TAG+="uaccess"
-
       ######################################################################
 
       # IPTS Touchscreen (SP2017)
@@ -107,6 +61,8 @@
 
       # IPTS Pen (SP2017)
       SUBSYSTEMS=="input", ATTRS{name}=="ipts 1B96:001F Pen", SYMLINK+="input/pen"
+
+      ######################################################################
     '';
 
     flatpak = { # for Spotify, Discord, and Telegram
