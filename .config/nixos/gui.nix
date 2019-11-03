8203a4bb837da781c59e7e7da7dbbc0bb61b4afd
hpfr <44043764+hpfr@users.noreply.github.com>
2019-11-02 21:08:34 -0500

    Add OnlyKey udev rules

--- .config/nixos/gui.nix
+++ .config/nixos/gui.nix
@@ -80,6 +80,44 @@
 
     dbus.packages = with pkgs; [ gnome3.dconf ];
 
+    udev.extraRules = ''
+      # UDEV Rules for OnlyKey, https://docs.crp.to/linux.html
+      #
+      # This file must be placed at:
+      #
+      # /etc/udev/rules.d/49-onlykey.rules    (preferred location)
+      #   or
+      # /lib/udev/rules.d/49-onlykey.rules    (req'd on some broken systems)
+      #
+      # To install, type this command in a terminal:
+      #   sudo cp 49-onlykey.rules /etc/udev/rules.d/49-onlykey.rules
+      #
+      # After this file is installed, physically unplug and reconnect OnlyKey.
+      #
+      ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", ENV{ID_MM_DEVICE_IGNORE}="1"
+      ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", ENV{MTP_NO_PROBE}="1"
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", MODE:="0666"
+      KERNEL=="ttyACM*", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", MODE:="0666"
+      #
+      # If you share your linux system with other users, or just don't like the
+      # idea of write permission for everybody, you can replace MODE:="0666" with
+      # OWNER:="yourusername" to create the device owned by you, or with
+      # GROUP:="somegroupname" and mange access using standard unix groups.
+      #
+      # One requirement of TOTP (Time-based One-time Password) is having the correct
+      # time. If OnlyKey is used on a system where the OnlyKey app is not running it
+      # will display “NOTSET” instead of the OTP code. Because OnlyKey has no battery
+      # it requires an app to send it the correct time to be able to generate TOTP
+      # codes. If you have OnlyKey command-line utility installed, adding the
+      # following will automatically set the current time on OnlyKey every time you
+      # plug it: RUN+="/usr/local/bin/onlykey-cli settime"
+      #
+      # SUBSYSTEMS=="usb", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", MODE:="0660", GROUP:="onlykey", RUN+="/usr/local/bin/onlykey-cli settime"
+      # KERNEL=="ttyACM*", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", MODE:="0660", GROUP:="onlykey", RUN+="/usr/local/bin/onlykey-cli settime"
+      #
+      ##
+    '';
+
     redshift = {
       enable = true;
       temperature.night = 3000;
