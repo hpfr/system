a00f20c4d96f1c5f1c49c6045c9aac3c63e10c16
hpfr <44043764+hpfr@users.noreply.github.com>
2019-11-03 07:04:12 -0600

    Modularize neovim config with home-manager
    Also remove a bunch of cruft from the config, I barely ever use neovim
    so I don't need any powerful features

--- .config/nixos/base.nix
+++ .config/nixos/base.nix
@@ -277,7 +277,34 @@
         '';
       };
 
-      neovim.enable = true;
+      neovim = {
+        enable = true;
+        extraConfig = ''
+          set bg=light
+          set go=a
+          set mouse=a
+          set nohlsearch
+          set clipboard+=unnamedplus
+
+          " some basics
+            nnoremap c "_c
+            set nocompatible
+            filetype plugin on
+            syntax on
+            set encoding=utf-8
+            set number relativenumber
+          " enable autocompletion
+            set wildmode=longest,list,full
+          " disable automatic commenting on newline
+            autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
+
+          " alias replace all to S
+            nnoremap S :%s//g<Left><Left>
+
+          " automatically delete all trailing whitespace on save
+            autocmd BufWritePre * %s/\s\+$//e
+        '';
+      };
 
       emacs = {
         enable = true;
