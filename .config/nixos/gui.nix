e2d5280f7ebd9c1bd1aae552dc36ca8efeb3910e
hpfr <44043764+hpfr@users.noreply.github.com>
2019-11-03 07:07:40 -0600

    Add mpv config to home-manager

--- .config/nixos/gui.nix
+++ .config/nixos/gui.nix
@@ -314,7 +314,21 @@
         '';
       };
 
-      mpv.enable = true;
+      mpv = {
+        enable = true;
+        bindings = {
+          h = "seek -5";
+          j = "seek -60";
+          k = "seek 60";
+          l = "seek 5";
+
+          # rebind lost l binding, matches across from L which loops whole file
+          H = "ab-loop";
+          # rebind lost j binding, move J to K
+          J = "cycle sub";
+          K = "cycle sub down";
+        };
+      };
     };
 
     services = {
