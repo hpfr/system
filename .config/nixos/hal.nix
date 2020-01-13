e5080f15633ad487a299afd7284aa492c8fdf156
hpfr <44043764+hpfr@users.noreply.github.com>
2020-01-13 15:03:08 -0600

    Temporarily disable network config
    I should probably be doing this on my router so I can easily travel with
    just the PC and not have to disable the static IP.

--- .config/nixos/hal.nix
+++ .config/nixos/hal.nix
@@ -9,15 +9,15 @@
 
   networking = {
     hostName = "hal";
-    interfaces.enp4s0.ipv4.addresses = [{
-      address = "192.168.1.8";
-      prefixLength = 24;
-    }];
-    defaultGateway = {
-      address = "192.168.1.1";
-      interface = "enp4s0";
-    };
-    nameservers = [ "1.1.1.1" "8.8.8.8" ];
+    # interfaces.enp4s0.ipv4.addresses = [{
+    #   address = "192.168.1.8";
+    #   prefixLength = 24;
+    # }];
+    # defaultGateway = {
+    #   address = "192.168.1.1";
+    #   interface = "enp4s0";
+    # };
+    # nameservers = [ "1.1.1.1" "8.8.8.8" ];
   };
 
   nix.extraOptions = ''
