843b65bf75590849035a58234369babbbd91ecaa
hpfr <44043764+hpfr@users.noreply.github.com>
2020-01-13 14:43:47 -0600

    Update console attribute names

--- .config/nixos/base.nix
+++ .config/nixos/base.nix
@@ -107,14 +107,12 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Select internationalisation properties.
-  i18n = {
-    # consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
+  console = {
+    # font = "Lat2-Terminus16";
+    keyMap = "us";
   };
 
-  # Set your time zone.
+  i18n.defaultLocale = "en_US.UTF-8";
   time.timeZone = "America/Chicago";
 
   # List packages installed in system profile. To search, run:
