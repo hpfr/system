f2933cbe2eef6a864a1ca1a79560b77e6748e94f
hpfr <44043764+hpfr@users.noreply.github.com>
2020-01-23 01:37:43 -0600

    Improve readline config
    Tweaks from Arch Wiki

--- .config/nixos/base.nix
+++ .config/nixos/base.nix
@@ -220,13 +220,26 @@
 
       readline = {
         enable = true;
-        variables.editing-mode = "vi";
+        variables = {
+          show-mode-in-prompt = true;
+          editing-mode = "vi";
+        };
+        bindings = {
+          "\\e[A" = "history-search-backward";
+          "\\e[B" = "history-search-forward";
+        };
         extraConfig = ''
-          $if mode=vi
-            set show-mode-in-prompt on
+          $if term=linux
+            set vi-ins-mode-string \1\e[?0c\2
+            set vi-cmd-mode-string \1\e[?8c\2
+          $else
             set vi-ins-mode-string \1\e[6 q\2
             set vi-cmd-mode-string \1\e[2 q\2
           $endif
+          set keymap vi-command
+          # these are for vi-command mode
+          j: history-search-forward
+          k: history-search-backward
         '';
       };
 
