269263e3c21324b565aafc4ed095d51b21f61f75
hpfr <44043764+hpfr@users.noreply.github.com>
2020-05-25 03:50:23 -0500

    Update fontconfig and default fonts
    - Move to Iosevka from Hasklug Nerd Font
    - Remove extraneous fontconfig rules
    - Add Iosevka custom build overlays
    - Use noto-fonts and lato rather than 800MiB google-fonts package
    - Use single Iosevka Nerd Font overlay rather than 4GiB Nerd Fonts
      package

--- gui.nix
+++ gui.nix
@@ -8,6 +8,94 @@
     (self: super: {
       keepassxc = super.keepassxc.override { withKeePassNetworking = true; };
     })
+    (self: super:
+      let
+        characterOverrides = {
+          # applies to all three slopes
+          design = [
+            "v-zero-dotted"
+            "v-asterisk-hexlow"
+            "v-paragraph-low"
+            "v-at-short"
+          ];
+
+          upright = [ "v-g-doublestorey" ];
+          italic = [ ];
+          oblique = [ ];
+        };
+        mkIosevka =
+          { family, design ? [ ], upright ? [ ], italic ? [ ], oblique ? [ ] }:
+          super.iosevka.override {
+            set = builtins.replaceStrings [ " " ] [ "-" ] (lib.toLower family);
+            privateBuildPlan = {
+              family = family;
+              design = design ++ characterOverrides.design;
+              upright = upright ++ characterOverrides.upright;
+              italic = italic ++ characterOverrides.italic;
+              oblique = oblique ++ characterOverrides.oblique;
+            };
+          };
+      in {
+        iosevka = mkIosevka { family = "Iosevka"; };
+        iosevka-term = mkIosevka {
+          family = "Iosevka Term";
+          design = [ "sp-term" ];
+        };
+        iosevka-slab = mkIosevka {
+          family = "Iosevka Slab";
+          design = [ "slab" ];
+        };
+        iosevka-term-slab = mkIosevka {
+          family = "Iosevka Term Slab";
+          design = [ "sp-term" "slab" ];
+        };
+        iosevka-curly = mkIosevka {
+          family = "Iosevka Curly";
+          design = "ss20";
+        };
+        iosevka-term-curly = mkIosevka {
+          family = "Iosevka Term Curly";
+          design = [ "sp-term" "ss20" ];
+        };
+        iosevka-curly-slab = mkIosevka {
+          family = "Iosevka Curly Slab";
+          design = [ "slab" "ss20" ];
+        };
+        iosevka-term-curly-slab = mkIosevka {
+          family = "Iosevka Term Curly Slab";
+          design = [ "sp-term" "slab" "ss20" ];
+        };
+        iosevka-etoile = mkIosevka {
+          family = "Iosevka Etoile";
+          design = [
+            "type"
+            "slab"
+            "v-at-fourfold"
+            "v-j-serifed"
+            "no-cv-ss"
+            "no-ligation"
+          ];
+          upright = [ "v-i-serifed" "v-l-serifed" ];
+          italic = [ "v-i-italic" "v-l-italic" ];
+          oblique = [ "v-i-serifed" "v-l-serifed" ];
+        };
+        iosevka-sparkle = mkIosevka {
+          family = "Iosevka Sparkle";
+          design = [
+            "type"
+            "v-at-fourfold"
+            "v-j-narrow-serifed"
+            "no-cv-ss"
+            "no-ligation"
+          ];
+          upright = [ "v-i-serifed" "v-l-serifed" "v-f-serifed" "v-r-serifed" ];
+          italic = [ "v-i-italic" "v-l-italic" "v-f-tailed" "v-r-top-serifed" ];
+          oblique = [ "v-i-serifed" "v-l-serifed" "v-f-serifed" "v-r-serifed" ];
+        };
+      })
+    (self: super: {
+      iosevka-nerd-font = super.nerdfonts.override { fonts = [ "Iosevka" ]; };
+    })
     (self: super: { xwallpaper = super.callPackage ./pkgs/xwallpaper { }; })
     (self: super: {
       gui-scripts = (super.runCommand "gui-scripts" {
@@ -25,25 +113,28 @@
 
   location.provider = "geoclue2"; # for redshift
 
-  fonts.enableDefaultFonts = false;
-  fonts.fonts = with pkgs; [
-    # lato, noto serif
-    google-fonts
-    # mono fonts with icons baked in
-    # warning: downloads almost 2 GiB
-    nerdfonts
-    # emoji
-    joypixels
-    # pseudo-bitmap
-    undefined-medium
-  ];
+  fonts = {
+    enableDefaultFonts = false;
+    fonts = with pkgs; [
+      noto-fonts # noto serif
+      lato # sans-serif
+      iosevka-term-curly-slab # monospace
+      joypixels # emoji
+      undefined-medium # pseudo-bitmap
 
-  # fonts.fontconfig.penultimate.enable = true;
-  fonts.fontconfig.defaultFonts = {
-    serif = [ "Noto Serif" ];
-    sansSerif = [ "Lato" ];
-    monospace = [ "Hasklug Nerd Font" ];
-    emoji = [ "JoyPixels" ];
+      # TODO: https://github.com/ryanoasis/nerd-fonts/issues/479
+      # https://github.com/ryanoasis/nerd-fonts/pull/461
+      iosevka-nerd-font # icons
+
+    ];
+
+    # TODO: try copying this option's functionality into home-manager module
+    fontconfig.defaultFonts = {
+      serif = [ "Noto Serif" ];
+      sansSerif = [ "Lato" ];
+      monospace = [ "Iosevka Term Curly Slab" ];
+      emoji = [ "JoyPixels" ];
+    };
   };
 
   # Enable sound.
