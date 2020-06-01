1c3be8a969ca0296d4bb9e5f7e1e34a16c61513b
hpfr <44043764+hpfr@users.noreply.github.com>
2020-05-31 22:26:06 -0500

    Fix hiDPI environment variable types
    It's complaining they're not strings now

--- profiles/system/hidpi.nix
+++ profiles/system/hidpi.nix
@@ -10,9 +10,9 @@ in {
     fonts.fontconfig.dpi = 192;
 
     environment.variables = {
-      GDK_SCALE = 2;
-      GDK_DPI_SCALE = 0.5;
-      QT_AUTO_SCREEN_SCALE_FACTOR = 1;
+      GDK_SCALE = "2";
+      GDK_DPI_SCALE = "0.5";
+      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
     };
   };
 }
