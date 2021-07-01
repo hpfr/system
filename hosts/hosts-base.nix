7d197d69a62f4480014b25afd026e13af3fa9b08
hpfr <44043764+hpfr@users.noreply.github.com>
2021-07-01 16:36:19 +0000

    Add basic secrets management
    I'm just using an attribute set that I don't commit for some basic
    private information like IP addresses. I'm okay with these values being
    in the Nix store; I just don't want them on the internet for some minor
    peace of mind.

--- hosts/hosts-base.nix
+++ hosts/hosts-base.nix
@@ -1,13 +1,18 @@
 { config, lib, pkgs, ... }:
 
-{
+let secrets = import ../lib/secrets.nix;
+in {
+  _module.args.secrets = secrets;
   imports = [ /etc/nixos/hardware-configuration.nix <home-manager/nixos> ]
     ++ (map (name: (../profiles/system + "/${name}"))
       (builtins.attrNames (builtins.readDir ./../profiles/system)));
 
   home-manager.users.lh = let nixos-config = config;
   in {
-    _module.args.nixos-config = nixos-config;
+    _module.args = {
+      inherit secrets;
+      nixos-config = nixos-config;
+    };
 
     imports = (map (name: (../profiles/user + "/${name}"))
       (builtins.attrNames (builtins.readDir ../profiles/user)));
