aebf376d643a8bfd4c523012113b931116a367ce
Liam Hupfer <liam@hpfr.net>
2021-07-02 17:05:55 -0500

    Set less colors in interactive shells only
    Fixing session variables recently caused these tput calls to break
    non-interactive shells for remote builds.

--- profiles/user/base.nix
+++ profiles/user/base.nix
@@ -53,23 +53,6 @@ in {
         # https://www.topbug.net/blog/2016/09/27/make-gnu-less-more-powerful/
         LESS =
           "--quit-if-one-screen --ignore-case --status-column --LONG-PROMPT --RAW-CONTROL-CHARS --HILITE-UNREAD --tabs=4 --no-init --window=-2";
-        # less colors
-        # https://unix.stackexchange.com/questions/119/colors-in-man-pages/147#147
-        # https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
-        LESS_TERMCAP_mb = "$(tput bold; tput setaf 2)"; # green
-        LESS_TERMCAP_md = "$(tput bold; tput setaf 6)"; # cyan
-        LESS_TERMCAP_me = "$(tput sgr0)";
-        LESS_TERMCAP_so =
-          "$(tput bold; tput setaf 3; tput setab 4)"; # yellow on blue
-        LESS_TERMCAP_se = "$(tput rmso; tput sgr0)";
-        LESS_TERMCAP_us = "$(tput smul; tput bold; tput setaf 7)"; # white
-        LESS_TERMCAP_ue = "$(tput rmul; tput sgr0)";
-        LESS_TERMCAP_mr = "$(tput rev)";
-        LESS_TERMCAP_mh = "$(tput dim)";
-        LESS_TERMCAP_ZN = "$(tput ssubm)";
-        LESS_TERMCAP_ZV = "$(tput rsubm)";
-        LESS_TERMCAP_ZO = "$(tput ssupm)";
-        LESS_TERMCAP_ZW = "$(tput rsupm)";
       };
     };
 
--- profiles/user/bash.nix
+++ profiles/user/bash.nix
@@ -35,10 +35,29 @@ in {
         ffplay = "ffplay -hide_banner";
         ffprobe = "ffprobe -hide_banner";
       };
+      # keep tput in interactive commands, otherwise it can break things using
+      # non-interactive shells like remote builds
       initExtra = ''
         stty -ixon # disable ctrl-s and ctrl-q
         # https://wiki.archlinux.org/index.php/Bash/Prompt_customization
         export PS1="\[$(tput bold)\]\[$(tput setaf 1)\][\[$(tput setaf 3)\]\u\[$(tput setaf 2)\]@\[$(tput setaf 4)\]\h \[$(tput setaf 5)\]\W\[$(tput setaf 1)\]]\[$(tput setaf 7)\]\\$ \[$(tput sgr0)\]"
+
+        # less colors
+        # https://unix.stackexchange.com/questions/119/colors-in-man-pages/147#147
+        # https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+        export LESS_TERMCAP_mb="$(tput bold; tput setaf 2)" # green
+        export LESS_TERMCAP_md="$(tput bold; tput setaf 6)" # cyan
+        export LESS_TERMCAP_me="$(tput sgr0)"
+        export LESS_TERMCAP_so="$(tput bold; tput setaf 3; tput setab 4)" # yellow on blue
+        export LESS_TERMCAP_se="$(tput rmso; tput sgr0)"
+        export LESS_TERMCAP_us="$(tput smul; tput bold; tput setaf 7)" # white
+        export LESS_TERMCAP_ue="$(tput rmul; tput sgr0)"
+        export LESS_TERMCAP_mr="$(tput rev)"
+        export LESS_TERMCAP_mh="$(tput dim)"
+        export LESS_TERMCAP_ZN="$(tput ssubm)"
+        export LESS_TERMCAP_ZV="$(tput rsubm)"
+        export LESS_TERMCAP_ZO="$(tput ssupm)"
+        export LESS_TERMCAP_ZW="$(tput rsupm)"
       '';
     };
   };
