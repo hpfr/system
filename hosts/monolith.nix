9c2acd7f0977cada1f7d0652ca4fdba302e2b39e
Liam Hupfer <liam@hpfr.net>
2021-07-05 00:57:31 -0500

    monolith: Remove vfio-pci binding at boot
    I've learned that libvirt handles unbinding device drivers and binding
    vfio-pci on VM startup. None of this is actually necessary in my case.
    It could be necessary if GPU reset bugs were an issue or if my host had
    a graphical session as well, but that's not the case for me.

--- hosts/monolith.nix
+++ hosts/monolith.nix
@@ -15,72 +15,11 @@
       # fan control (k10temp for CPU automatically loaded)
       "nct6775"
     ];
-
-    extraModprobeConfig = ''
-      # rtx gpu
-      alias pci:v000010DEd00001F02sv00001458sd000037C8bc03sc00i00 vfio-pci
-      # bpx pro
-      alias pci:v00001987d00005012sv00001987sd00005012bc01sc08i02 vfio-pci
-      # front usb
-      # alias pci:v00001022d000043D0sv00001849sd000043D0bc0Csc03i30 vfio-pci
-      # rear usb
-      alias pci:v00001022d0000145Fsv00001849sd0000FFFFbc0Csc03i30 vfio-pci
-
-      # softdep nouveau pre: vfio-pci
-      # softdep i2c_nvidia_gpu pre: vfio-pci
-
-      # 21:00.0, .1, .2, .3 in IOMMU group 13
-      # 22:00.0, .1 for second slot (580)
-      # 20:00.0 for second M.2 slot (BPX Pro)
-      # 03:00.0 for front USB controller: 1022:43d0
-      # 23:00.3 for rear USB controller
-      options vfio-pci ids=10de:1f02,10de:10f9,10de:1ada,10de:1adb,1002:67df,1002:aaf0,1987:5012,1022:145f
-    '';
   };
 
-  systemd = {
-    services = let
-      # unbind a PCI device from a particular driver and bind it to vfio-pci
-      # this is useful when drivers are built into the kernel so they load
-      # before a module like vfio-pci
-      mkUnbindService = { enable, device-id, driver }: {
-        inherit enable;
-        wantedBy = [ "default.target" ];
-        serviceConfig.Type = "oneshot";
-
-        script = ''
-          if test -d '/sys/bus/pci/drivers/${driver}/${device-id}'; then
-            echo -n "${device-id}" > /sys/bus/pci/drivers/${driver}/unbind
-            echo -n "${device-id}" > /sys/bus/pci/drivers/vfio-pci/bind
-          fi
-        '';
-      };
-    in {
-      unbind-rtx-usb = mkUnbindService {
-        enable = true;
-        device-id = "0000:21:00.2";
-        driver = "xhci_hcd";
-      };
-      unbind-bpx-ssd = mkUnbindService {
-        enable = true;
-        device-id = "0000:20:00.0";
-        driver = "nvme";
-      };
-      unbind-front-usb = mkUnbindService {
-        enable = false;
-        device-id = "0000:03:00.0";
-        driver = "xhci_hcd";
-      };
-      unbind-rear-usb = mkUnbindService {
-        enable = true;
-        device-id = "0000:23:00.3";
-        driver = "xhci_hcd";
-      };
-    };
-    # https://askubuntu.com/questions/676007/how-do-i-make-my-systemd-service-run-via-specific-user-and-start-on-boot
-    # https://serverfault.com/questions/846441/loginctl-enable-linger-disable-linger-but-reading-linger-status
-    tmpfiles.rules = [ "f /var/lib/systemd/linger/lh 0644 root root" ];
-  };
+  # https://askubuntu.com/questions/676007/how-do-i-make-my-systemd-service-run-via-specific-user-and-start-on-boot
+  # https://serverfault.com/questions/846441/loginctl-enable-linger-disable-linger-but-reading-linger-status
+  systemd.tmpfiles.rules = [ "f /var/lib/systemd/linger/lh 0644 root root" ];
 
   networking = {
     hostName = "monolith";
