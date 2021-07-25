33a1bc1392b98b78c869fc4ac8e35a4adaee9f20
Liam Hupfer <liam@hpfr.net>
2021-07-25 02:51:31 -0500

    Enable direnv
    It's a real game changer for isolating development environments from my
    global packages

--- profiles/user/base.nix
+++ profiles/user/base.nix
@@ -80,6 +80,11 @@ in {
         };
       };
 
+      direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+      };
+
       git = {
         enable = true;
         package = pkgs.gitAndTools.gitFull;
