d491600ac7d201aa490a8dcd12b9d1667fb64103
Liam Hupfer <liam@hpfr.net>
2021-08-11 11:04:02 -0500

    emacs: Add ement
    It's a workable client now, albeit without encryption support.

--- profiles/user/emacs/doom/packages.el
+++ profiles/user/emacs/doom/packages.el
@@ -5,8 +5,7 @@
 (package! disk-usage)
 (package! ztree :pin "f05677f9696e573c8c607e8876fb4a0cccbc491f")
 (when (package! ement
-        :recipe (:host github :repo "alphapapa/ement.el")
-        :disable t)
+        :recipe (:host github :repo "alphapapa/ement.el"))
   (package! plz
     :recipe (:host github :repo "alphapapa/plz.el")))
 (package! elpher)
