b1b45da21caafcd1fdcbc010fac8a3ae9f542b35
Liam Hupfer <liam@hpfr.net>
2022-01-19 15:10:23 -0600

    emacs: Restore org-agenda category
    Thanks to
    https://d12frosted.io/posts/2020-06-24-task-management-with-roam-vol2.html.
    Hopefully org-roam's coming agenda features make this all moot before I
    get around to streamlining it.

--- profiles/user/emacs/doom/modules/lh/org/config.el
+++ profiles/user/emacs/doom/modules/lh/org/config.el
@@ -207,6 +207,40 @@
   :commands (org-super-agenda-mode))
 
 (after! org-agenda
+  ;; TODO: do this properly?
+  (require 'vulpea)
+
+  ;; better agenda todo prefix
+  (defun vulpea-agenda-category (&optional len)
+    "Get category of item at point for agenda.
+Category is defined by one of the following items:
+- CATEGORY property
+- TITLE keyword
+- TITLE property
+- filename without directory and extension
+When LEN is a number, resulting string is padded right with
+spaces and then truncated with ... on the right if result is
+longer than LEN.
+Usage example:
+  (setq org-agenda-prefix-format
+        '((agenda . \" %(vulpea-agenda-category) %?-12t %12s\")))
+Refer to `org-agenda-prefix-format' for more information."
+    (let* ((file-name (when buffer-file-name
+                        (file-name-sans-extension
+                         (file-name-nondirectory buffer-file-name))))
+           (title (vulpea-buffer-prop-get "title"))
+           (category (org-get-category))
+           (result
+            (or (if (and
+                     title
+                     (string-equal category file-name))
+                    title
+                  category)
+                "")))
+      (if (numberp len)
+          (s-truncate len (s-pad-right len " " result) "…")
+        result)))
+
   (setq
    ;; if I've explicitly scheduled a task with a deadline (maybe an assignment
    ;; doesn't release until the scheduled time), don't warn me until the
@@ -219,10 +253,10 @@
    org-agenda-todo-ignore-scheduled 'all
    ;; org-agenda-todo-ignore-with-date t
    org-agenda-prefix-format
-   '((agenda . " %i %?-12t% s")
-     (todo . " %i ")
-     (tags . " %i ")
-     (search . " %i ")))
+   '((agenda . " %i %-12(vulpea-agenda-category 12)%?-12t% s")
+     (todo . " %i %-12(vulpea-agenda-category 12) ")
+     (tags . " %i %-12(vulpea-agenda-category 12) ")
+     (search . " %i %-12(vulpea-agenda-category 12) ")))
 
   (org-super-agenda-mode))
 
--- profiles/user/emacs/doom/modules/lh/org/packages.el
+++ profiles/user/emacs/doom/modules/lh/org/packages.el
@@ -7,3 +7,6 @@
 (package! org-chef)
 (when (featurep! :lang org +present)
   (package! revealjs-plugins-rajgoel :recipe (:host github :repo "rajgoel/reveal.js-plugins" :files ("chalkboard"))))
+;; convenient org and roam extensions by d12frosted, can probably be removed in
+;; favor of my own implementations in future, since I don't use much
+(package! vulpea)
