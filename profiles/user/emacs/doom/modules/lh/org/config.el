19a947ea104bdc43f7cc619974a3b089477996e4
Liam Hupfer <liam@hpfr.net>
2022-03-13 03:18:07 -0500

    emacs: Improve org-roam-node-slug

--- profiles/user/emacs/doom/modules/lh/org/config.el
+++ profiles/user/emacs/doom/modules/lh/org/config.el
@@ -365,31 +365,9 @@ Refer to `org-agenda-prefix-format' for more information."
 ;; use hyphens instead of underscores in roam filenames
 (after! org-roam-node
   (cl-defmethod org-roam-node-slug ((node org-roam-node))
-    (let ((title (org-roam-node-title node))
-          (slug-trim-chars '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
-                             768 ; U+0300 COMBINING GRAVE ACCENT
-                             769 ; U+0301 COMBINING ACUTE ACCENT
-                             770 ; U+0302 COMBINING CIRCUMFLEX ACCENT
-                             771 ; U+0303 COMBINING TILDE
-                             772 ; U+0304 COMBINING MACRON
-                             774 ; U+0306 COMBINING BREVE
-                             775 ; U+0307 COMBINING DOT ABOVE
-                             776 ; U+0308 COMBINING DIAERESIS
-                             777 ; U+0309 COMBINING HOOK ABOVE
-                             778 ; U+030A COMBINING RING ABOVE
-                             780 ; U+030C COMBINING CARON
-                             795 ; U+031B COMBINING HORN
-                             803 ; U+0323 COMBINING DOT BELOW
-                             804 ; U+0324 COMBINING DIAERESIS BELOW
-                             805 ; U+0325 COMBINING RING BELOW
-                             807 ; U+0327 COMBINING CEDILLA
-                             813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
-                             814 ; U+032E COMBINING BREVE BELOW
-                             816 ; U+0330 COMBINING TILDE BELOW
-                             817 ; U+0331 COMBINING MACRON BELOW
-                             )))
+    (let ((title (org-roam-node-title node)))
       (cl-flet* ((nonspacing-mark-p (char)
-                                    (memq char slug-trim-chars))
+                                    (memq char ucs-normalize-combining-chars))
                  (strip-nonspacing-marks (s)
                                          (ucs-normalize-NFC-string
                                           (apply #'string (seq-remove #'nonspacing-mark-p
@@ -397,9 +375,9 @@ Refer to `org-agenda-prefix-format' for more information."
                  (cl-replace (title pair)
                              (replace-regexp-in-string (car pair) (cdr pair) title)))
         (let* ((pairs `(("[^[:alnum:][:digit:]]" . "-") ; convert anything not alphanumeric
-                        ("--*" . "-")                   ; remove sequential hyphens
-                        ("^-" . "")                     ; remove starting hyphen
-                        ("-$" . "")))                   ; remove ending hyphen
+                        ("--*" . "-")   ; remove sequential hyphens
+                        ("^-" . "")     ; remove starting hyphen
+                        ("-$" . "")))   ; remove ending hyphen
                (slug (-reduce-from #'cl-replace (strip-nonspacing-marks title) pairs)))
           (downcase slug))))))
 
