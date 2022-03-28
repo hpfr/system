5a75b8339ff95ee71ed32540794df9507e9bf966
Liam Hupfer <liam@hpfr.net>
2022-03-28 13:53:39 -0500

    emacs: Update org-id-link-to-org-use-id
    I think org-capture was creating extraneous ID's, as the docstring
    describes.

--- profiles/user/emacs/doom/modules/lh/org/config.el
+++ profiles/user/emacs/doom/modules/lh/org/config.el
@@ -37,8 +37,7 @@
           ("\\.mkv\\'" . "mpv %s")
           (system . "xdg-open %s")
           (t . "xdg-open %s"))
-        ;; always use UUID's in org headline links
-        org-id-link-to-org-use-id t
+        org-id-link-to-org-use-id 'create-if-interactive
         org-use-property-inheritance '("ROAM_EXCLUDE")
         org-startup-folded 'showall
         ;; no shouting
