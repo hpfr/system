9d5d2e25aa6e80e5fda6d27dd69b5eeecfbae317
Liam Hupfer <liam@hpfr.net>
2022-04-11 12:36:22 -0500

    Remove ztree pin
    It's not a library, and I don't really depend on it.

--- profiles/user/emacs/doom/packages.el
+++ profiles/user/emacs/doom/packages.el
@@ -14,7 +14,7 @@
 (package! lexic)
 (package! disk-usage)
 (package! daemons)
-(package! ztree :pin "f05677f9696e573c8c607e8876fb4a0cccbc491f")
+(package! ztree)
 (package! tldr)
 (package! dired-dragon                  ; drag and drop
   :recipe (:host github :repo "jeetelongname/dired-dragon")
