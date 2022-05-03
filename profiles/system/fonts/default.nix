a9e9a5a28d0225e76967ebef2a1d7ea553ec4dd5
Liam Hupfer <liam@hpfr.net>
2022-05-03 14:16:25 -0500

    Add fonts via home-manager
    fonts.fontconfig.enable handles caching even though fonts are installed
    via the more generic home.packages (instead of a dedicated option like
    fonts.fonts). So there's little reason these need to be system-wide

--- profiles/system/fonts/default.nix
+++ profiles/system/fonts/default.nix
@@ -27,28 +27,6 @@ in {
         })
       ];
     };
-    fonts = {
-      enableDefaultFonts = false;
-      fonts = with pkgs; [
-        noto-fonts # no tofu
-        noto-fonts-extra # all weights
-        last-resort # final fallback
-        source-serif
-        source-sans
-        source-code-pro
-        stix-two
-        libertinus
-        liberation_ttf # kaobook
-        iosevka-term # monospace
-        iosevka-term-curly-slab # monospace serif for emacs
-        julia-mono # monospaced unicode
-        noto-fonts-emoji # emoji
-        undefined-medium # pseudo-bitmap
-        # https://github.com/adobe-fonts/source-han-mono/issues/1
-        source-han-sans # CJK support
-        source-han-serif
-        source-han-mono
-      ];
-    };
+    fonts.enableDefaultFonts = false;
   };
 }
--- profiles/user/fontconfig/default.nix
+++ profiles/user/fontconfig/default.nix
@@ -26,5 +26,27 @@ in {
       };
       "org/gnome/desktop/wm/preferences".titlebar-uses-system-font = true;
     };
+
+    home.packages = with pkgs; [
+      source-serif
+      source-sans
+      source-code-pro
+      stix-two
+      libertinus
+      liberation_ttf # kaobook
+      iosevka-term # monospace
+      iosevka-term-curly-slab # monospace serif for emacs
+      julia-mono # monospaced unicode
+      noto-fonts-emoji # emoji
+      undefined-medium # pseudo-bitmap
+
+      # https://github.com/adobe-fonts/source-han-mono/issues/1
+      source-han-sans # CJK support
+      source-han-serif
+      source-han-mono
+      noto-fonts # no tofu
+      noto-fonts-extra # all weights
+      last-resort # final fallback
+    ];
   };
 }
