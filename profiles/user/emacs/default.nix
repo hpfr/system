4a3956c5c821a27ebc03b505b9bc66cbdfb9d200
Liam Hupfer <liam@hpfr.net>
2022-06-28 16:06:23 -0500

    Remove bookmarks Promnesia source
    This file is part of my exocortex now.

--- profiles/user/emacs/default.nix
+++ profiles/user/emacs/default.nix
@@ -127,25 +127,7 @@ in {
         "promnesia/config.py".text = ''
           from promnesia.common import Source
           from promnesia.sources import auto
-
-          ''''
-          List of sources to use.
-
-          You can specify your own, add more sources, etc.
-          See https://github.com/karlicoss/promnesia#setup for more information
-          ''''
-          SOURCES = [
-              Source(
-                  auto.index,
-                  '~/exocortex',
-                  name='exocortex'
-              ),
-              Source(
-                  auto.index,
-                  '~/nc/bookmarks.org',
-                  name='bookmarks dump'
-              )
-          ]
+          SOURCES = [Source(auto.index, '~/exocortex', name='exocortex')]
         '';
       };
       mimeApps = let
